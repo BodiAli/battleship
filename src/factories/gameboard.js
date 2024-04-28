c59da7b12b43f3b3cb1604b6150bfa28945024bd
Bodi_Ali <bodiali59@gmail.com>
2024-04-29 02:57:01 +0300

    Add coordinates array of objects

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -2,17 +2,31 @@ class Gameboard {
   constructor() {
     this.ships = [];
     this.missedCoordinates = [];
+    this.coordinates = [];
+    for (let x = 1; x <= 10; x++) {
+      for (let y = 1; y <= 10; y++) {
+        this.coordinates.push({ x, y });
+      }
+    }
+  }
+
+  placeShip(shipObject, xCoord, yCoord, direction) {
+    const targetCoord = this.coordinates.find((coord) => coord.x === xCoord && coord.y === yCoord);
+    if (targetCoord && !targetCoord.ship) {
+      targetCoord.ship = shipObject;
+      this.ships.push(shipObject);
+      return "ship placed";
+    }
+    return "could not place ship at this position";
   }
 
   receiveAttack(x, y) {
     let shipHit = false;
-    this.ships.forEach((ship) => {
-      ship.position.forEach((coords) => {
-        if (coords.x === x && coords.y === y) {
-          shipHit = true;
-          ship.hit();
-        }
-      });
+    this.coordinates.forEach((coords) => {
+      if (coords.x === x && coords.y === y && coords.ship) {
+        shipHit = true;
+        coords.ship.hit();
+      }
     });
     if (!shipHit) {
       this.missedCoordinates.push({ x, y });

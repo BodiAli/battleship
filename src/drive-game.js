d903c0f7ed9d03499e6eea7dbe55d6bad72c1ea3
Bodi_Ali <bodiali59@gmail.com>
2024-04-30 19:53:15 +0300

    create players when PVsC button clicked

--- src/drive-game.js
+++ src/drive-game.js
@@ -1,1 +1,12 @@
 import Player from "./factories/player.js";
+import Gameboard from "./factories/gameboard.js";
+import createCells from "./dom/create-cells.js";
+import Ship from "./factories/ship.js";
+
+const playerVsComputerButton = document.querySelector("button.pvs-btn.computer");
+
+playerVsComputerButton.addEventListener("click", () => {
+  const player = new Player();
+  const computer = new Player();
+  console.log(player, computer);
+});

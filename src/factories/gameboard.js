8e1f6dbb1abc5fd128637efeed5305fdfd51aff7
Bodi_Ali <bodiali59@gmail.com>
2024-05-01 22:20:14 +0300

    Refactor Gameboard placeShips method
    placeShips now effectively checks for ships at
    adjacent coordinates

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -5,63 +5,54 @@ class Gameboard {
     this.ships = [];
     this.missedCoordinates = [];
     this.coordinates = [];
-    for (let y = 1; y <= 10; y++) {
-      for (let x = 1; x <= 10; x++) {
-        this.coordinates.push({ x, y });
+    for (let x = 1; x <= 10; x++) {
+      for (let y = 1; y <= 10; y++) {
+        this.coordinates.push({ x, y, ship: null });
       }
     }
   }
 
-  placeShip(shipObject, xCoord, yCoord, direction) {
-    if (direction === "horizontal") {
-      let nextNextX = shipObject.length;
-      if (xCoord === 7 && shipObject.length === 4) {
-        nextNextX = 3;
-      } else if (xCoord === 8 && shipObject.length === 4) {
-        return "could not place ship at this position";
-      } else if (xCoord === 9 && (shipObject.length === 4 || shipObject.length === 3)) {
-        return "could not place ship at this position";
-      } else if (xCoord === 10 && shipObject.length !== 1) {
-        return "could not place ship at this position";
-      }
-      const targetCoord = this.coordinates.find((coord) => coord.x === xCoord && coord.y === yCoord);
-      if (targetCoord && !targetCoord.ship && this.checkAdjacentCoordinates(xCoord, yCoord)) {
-        if (shipObject.length === 1) {
-          this.ships.push(shipObject);
-          targetCoord.ship = shipObject;
-          return "ship placed";
-        }
-        for (let i = 1; i < shipObject.length; i++) {
-          const nextX = xCoord + i;
-          const nextCoord = this.coordinates.find((val) => val.x === nextX && val.y === yCoord);
-          const nextNextCoord = this.coordinates.find(
-            (val) => val.x === xCoord + nextNextX && val.y === yCoord
-          );
-          if (nextNextCoord.ship) {
-            break;
-          }
+  isCoordOccupied(x, y) {
+    return !!this.coordinates.find((coord) => coord.x === x && coord.y === y && coord.ship);
+  }
 
-          if (nextCoord && !nextCoord.ship && !nextNextCoord.ship) {
-            this.ships.push(shipObject);
-            targetCoord.ship = shipObject;
-            nextCoord.ship = shipObject;
-          } else {
-            return "could not place ship at this position";
-          }
-        }
-      }
-    } else if (direction === "vertical") {
-      for (let i = 1; i < shipObject.length; i++) {
-        const nextY = yCoord + i;
-        const nextCoord = this.coordinates.find((val) => val.x === xCoord && val.y === nextY);
-        if (nextCoord && !nextCoord.ship) {
-          nextCoord.ship = shipObject;
-        } else {
-          return "could not place ship at this position";
-        }
-      }
+  placeShip(shipObject, xCoord, yCoord, direction) {
+    const shipLength = shipObject.length;
+    const shipPositions = [];
+
+    // Check if the ship can be placed based on its length and direction
+    let isValidPlacement = false;
+    if (direction === "horizontal" && xCoord + shipLength - 1 <= 10) {
+      isValidPlacement = true;
+    } else if (direction === "vertical" && yCoord + shipLength - 1 <= 10) {
+      isValidPlacement = true;
     }
-    return "ship placed";
+
+    if (!isValidPlacement) {
+      console.log("Invalid ship placement: Ship cannot be placed at the specified coordinates or direction");
+      return;
+    }
+
+    for (let i = 0; i < shipLength; i++) {
+      const x = direction === "horizontal" ? xCoord + i : xCoord;
+      const y = direction === "vertical" ? yCoord + i : yCoord;
+      if (
+        this.isCoordOccupied(x, y) ||
+        this.isCoordOccupied(x - 1, y) ||
+        this.isCoordOccupied(x + 1, y) ||
+        this.isCoordOccupied(x, y - 1) ||
+        this.isCoordOccupied(x, y + 1)
+      ) {
+        console.log("Invalid ship placement: Adjacent coordinates already have ships.");
+        return;
+      }
+      shipPositions.push({ x, y });
+    }
+    shipPositions.forEach(({ x, y }) => {
+      const coordinate = this.coordinates.find((coord) => coord.x === x && coord.y === y);
+      coordinate.ship = shipObject;
+    });
+    this.ships.push(shipObject);
   }
 
   receiveAttack(x, y) {
@@ -81,70 +72,16 @@ class Gameboard {
   isAllSunk() {
     return this.ships.every((ship) => ship.hitCount >= ship.length);
   }
-
-  checkAdjacentCoordinates(xCoord, yCoord) {
-    let nextXCoord;
-    let nextYCoord;
-    let previousXCoord;
-    let previousYCoord;
-    if (xCoord === 1) {
-      previousXCoord = 1;
-      nextXCoord = xCoord + 1;
-    } else if (xCoord === 10) {
-      nextXCoord = 10;
-      previousXCoord = xCoord - 1;
-    } else {
-      previousXCoord = xCoord - 1;
-      nextXCoord = xCoord + 1;
-    }
-    if (yCoord === 1) {
-      previousYCoord = 1;
-      nextYCoord = yCoord + 1;
-    } else if (yCoord === 10) {
-      nextYCoord = 10;
-      previousYCoord = yCoord - 1;
-    } else {
-      previousYCoord = yCoord - 1;
-      nextYCoord = yCoord + 1;
-    }
-
-    const coordMinusXSameY = this.coordinates.find(
-      (coord) => coord.x === previousXCoord && coord.y === yCoord
-    );
-    const coordMinusXMinusY = this.coordinates.find(
-      (coord) => coord.x === previousXCoord && coord.y === previousYCoord
-    );
-    const coordMinusYSameX = this.coordinates.find(
-      (coord) => coord.x === xCoord && coord.y === previousYCoord
-    );
-
-    const coordPlusXSameY = this.coordinates.find((coord) => coord.x === nextXCoord && coord.y === yCoord);
-    const coordPlusXPlusY = this.coordinates.find(
-      (coord) => coord.x === nextXCoord && coord.y === nextYCoord
-    );
-    const coordSameXPlusY = this.coordinates.find((coord) => coord.x === xCoord && coord.y === nextYCoord);
-    if (
-      !coordMinusXSameY.ship &&
-      !coordMinusXMinusY.ship &&
-      !coordMinusYSameX.ship &&
-      !coordPlusXSameY.ship &&
-      !coordPlusXPlusY.ship &&
-      !coordSameXPlusY.ship
-    ) {
-      return true;
-    }
-
-    return false;
-  }
 }
 const gameBoard = new Gameboard();
 const ship1 = new Ship(3);
 
 const ship2 = new Ship(4);
 const ship3 = new Ship(1);
+const ship4 = new Ship(4);
 
 gameBoard.placeShip(ship1, 1, 3, "horizontal");
-console.log(gameBoard.placeShip(ship2, 10, 3, "horizontal"));
-console.log(gameBoard.placeShip(ship3, 10, 3, "horizontal"));
+console.log(gameBoard.placeShip(ship3, 8, 3, "horizontal"));
+console.log(gameBoard.placeShip(ship2, 6, 3, "horizontal"));
 console.log(gameBoard.coordinates);
 export default Gameboard;

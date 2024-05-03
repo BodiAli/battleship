acc2524dfb9ac2c0c5695bccd93b4fd523fcc707
Bodi_Ali <bodiali59@gmail.com>
2024-05-03 04:55:05 +0300

    Comment out adding predetermined classes

--- src/dom/create-cells.js
+++ src/dom/create-cells.js
@@ -4,9 +4,16 @@ function createCells(gameBoardObj1, gameBoardObj2, grid1, grid2) {
     cell.classList.add("cell");
     cell.coord = gameBoardObj1.coordinates[i];
     cell.player = "player1";
-    if (cell.coord.x === 3 && cell.coord.y === 3) {
-      cell.classList.add("cell-busy");
-    }
+    // if (cell.coord.x === 2 && cell.coord.y === 3) {
+    //   cell.classList.add("hit");
+    //   cell.classList.add("cell-busy");
+    // }
+    // if (cell.coord.x === 6 && cell.coord.y === 3) {
+    //   cell.classList.add("miss");
+    // }
+    // if (cell.coord.x === 10 && cell.coord.y === 3) {
+    //   cell.classList.add("adjacent-hit");
+    // }
     grid1.appendChild(cell);
   }
   for (let i = 0; i < gameBoardObj2.coordinates.length; i++) {

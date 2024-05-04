8fa8b159138b3bc9d2ea59bb5d830efaf4664272
Bodi_Ali <bodiali59@gmail.com>
2024-05-04 09:08:33 +0300

    Add integration tests for drive-game module

---
+++ src/tests/integration/drive-game.test.js
@@ -0,0 +1,25 @@
+import driveGame from "../../drive-game.js";
+
+describe("driveGame function", () => {
+  test("driveGame should get player names", () => {
+    const game = driveGame();
+    const players = game.getPlayers("Bodi", "Mazen");
+    expect(players.player1.name).toMatch("Bodi");
+    expect(players.player2.name).toMatch("Mazen");
+  });
+
+  test("driveGame should get ships", () => {
+    const game = driveGame();
+    const ships = game.getShips();
+    expect(ships).toHaveProperty("shipLength4");
+    expect(ships).toHaveProperty("ship1Length3");
+    expect(ships).toHaveProperty("ship2Length3");
+    expect(ships).toHaveProperty("ship1Length2");
+    expect(ships).toHaveProperty("ship2Length2");
+    expect(ships).toHaveProperty("ship3Length2");
+    expect(ships).toHaveProperty("ship1Length1");
+    expect(ships).toHaveProperty("ship2Length1");
+    expect(ships).toHaveProperty("ship3Length1");
+    expect(ships).toHaveProperty("ship4Length1");
+  });
+});

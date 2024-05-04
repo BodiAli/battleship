23f6be393a3809b490b9aa1d198d5b3bf5b8d44f
Bodi_Ali <bodiali59@gmail.com>
2024-05-04 09:13:01 +0300

    Create module for manipulating the game with events

---
+++ src/dom/dom-actions.js
@@ -0,0 +1,76 @@
+import driveGame from "../drive-game.js";
+import renderCells from "./render-cells.js";
+import createCells from "./create-cells.js";
+import choosePlayerVs from "./dom-pvs.js";
+
+class Dom {
+  static init() {
+    this.game = driveGame();
+    choosePlayerVs.playerVsComputer = true;
+    this.getPlayers();
+    this.grids = this.getGrids();
+    createCells(this.player1.gameBoard, this.player2.gameBoard, this.grids.grid1, this.grids.grid2);
+    this.cacheDom();
+    this.bindEvents();
+  }
+
+  static getGrids() {
+    const grid1 = document.querySelector("#player-grid");
+    let grid2 = document.querySelector("#player2-grid");
+    if (choosePlayerVs.playerVsComputer) {
+      grid2 = document.querySelector("#computer-grid");
+    }
+    return { grid1, grid2 };
+  }
+
+  static cacheDom() {
+    this.player1Cells = document.querySelectorAll("#player-grid > .cell");
+    this.player2Cells = document.querySelectorAll("#player2-grid > .cell");
+    if (choosePlayerVs.playerVsComputer) {
+      this.player2Cells = document.querySelectorAll("#computer-grid > .cell");
+    }
+  }
+
+  static bindEvents() {
+    this.player2Cells.forEach((cell) => {
+      cell.addEventListener("click", this.attackOpponent.bind(this));
+    });
+  }
+
+  static getPlayers() {
+    const players = this.game.getPlayers("bodi");
+    this.player1 = players.player1;
+    this.player2 = players.player2;
+
+    this.player2.gameBoard.placeShip(this.game.getShips().shipLength4, 3, 3, "vertical");
+    this.player1.gameBoard.placeShip(this.game.getShips().shipLength4, 3, 3, "vertical");
+  }
+
+  static attackOpponent(ev) {
+    if (!this.isGameOver()) {
+      const cell = ev.target;
+      if (!cell.isHit) {
+        this.player2.gameBoard.receiveAttack(cell.coord.x, cell.coord.y);
+        renderCells(this.player1Cells, this.player2Cells, this.player1.gameBoard, this.player2.gameBoard);
+        console.log(cell);
+      }
+      cell.isHit = true;
+      if (this.isGameOver()) {
+        ev.preventDefault();
+      }
+    }
+  }
+
+  static isGameOver() {
+    if (this.player1.gameBoard.isAllSunk()) {
+      return true;
+    }
+    if (this.player2.gameBoard.isAllSunk()) {
+      return true;
+    }
+    return false;
+  }
+}
+Dom.init();
+
+export default Dom;

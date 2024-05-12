a0ef5076dd0dbf10973fe488ca2d8031043651c2
Bodi_Ali <bodiali59@gmail.com>
2024-05-12 08:13:54 +0300

    Remove unused player property in cells

--- src/dom/create-cells.js
+++ src/dom/create-cells.js
@@ -3,14 +3,12 @@ function createCells(gameBoardObj1, gameBoardObj2, grid1, grid2) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.coord = gameBoardObj1.coordinates[i];
-    cell.player = "player1";
     grid1.appendChild(cell);
   }
   for (let i = 0; i < gameBoardObj2.coordinates.length; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.coord = gameBoardObj2.coordinates[i];
-    cell.player = "player2";
     grid2.appendChild(cell);
   }
 }

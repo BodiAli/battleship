2d072d981730dc4d82fc4af7ce9102dc0bac5f20
Bodi_Ali <bodiali59@gmail.com>
2024-05-21 20:44:16 +0300

    Import toggle-sound module to initialize variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/style.css";
 import "./styles/reset.css";
+import "./toggle-sound.js";
 import "./dom/dom-pvs.js";
 import "./dom/dom-manipulation-p-vs-c.js";
 import "./dom/dom-manipulation-p-vs-p.js";
